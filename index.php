8fac373f0b04c4bb88dad214616183ddf9f51d1d
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-04 08:45:10 +0200

    Completed table printer

--- index.php
+++ index.php
@@ -10,7 +10,19 @@
 
     <?php 
 
+        function printTable($rows, $cols) {
+            $outputTable = "<table style='border-collapse: collapse; font-size: 24px;'>";
+            for($i = 0; $i < $rows; $i++) {
+                $outputTable .= "<tr>";
+                for($j = 0; $j < $cols; $j++) {
+                    $outputTable .= "<th style='padding: 10px; border: black solid 1px;'> ($i;$j) </th>";
+                }
+                $outputTable .= "</tr>";
+            } 
+            return $outputTable .= "</table>";
+        }
 
+        echo printTable(rand(2,8), rand(2,8));
 
     ?>
 
